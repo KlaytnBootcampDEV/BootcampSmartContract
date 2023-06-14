283d335d8e5b814666eeb6671cee97f93dd6f56f
ThanhTrang-Phan <trangpt@vmodev.com>
2023-06-14 16:00:33 +0700

    update

--- 4. Tạo Dapp (Phần I): Viết 1 SC bỏ phiếu cộng đồng/contracts/VotingContract.sol
+++ 4. Tạo Dapp (Phần I): Viết 1 SC bỏ phiếu cộng đồng/contracts/VotingContract.sol
@@ -2,13 +2,16 @@ pragma solidity >0.8.9;
 
 interface IToken {
     function transferFrom(address, address, uint256) external returns (bool);
+
     function approve(address, uint256) external returns (bool);
-    function balanceOf(address) external returns(uint256);
+
+    function balanceOf(address) external returns (uint256);
+
     function totalSupply() external returns (uint256);
 }
 
 contract VotingContract {
-    struct Proposal{
+    struct Proposal {
         string description;
         uint256 yesCount;
         uint256 noCount;
@@ -19,7 +22,7 @@ contract VotingContract {
     IToken public votingToken;
     Proposal[] public proposals;
     uint256 public proposalCount;
-    
+
     mapping(address => mapping(uint256 => bool)) private hasVoted;
     mapping(uint256 => uint8) public resultProposal;
 
@@ -27,18 +30,21 @@ contract VotingContract {
     event CastVote(address, uint256, bool);
     event Finalize(uint256, uint8);
 
-    modifier checkProposalEnded(uint256 proposalId){
-        require (block.timestamp < proposals[proposalId].timestamp, "Proposal is ended");
+    modifier checkProposalEnded(uint256 proposalId) {
+        require(
+            block.timestamp < proposals[proposalId].timestamp,
+            "Proposal is ended"
+        );
         _;
     }
 
-    constructor(address _votingToken){
+    constructor(address _votingToken) {
         proposalCount = 0;
         votingToken = IToken(_votingToken);
     }
 
     function createProposal(string memory _description) public {
-        votingToken.transferFrom(msg.sender, address(this) , 20*10**18 );
+        votingToken.transferFrom(msg.sender, address(this), 20 * 10 ** 18);
         Proposal memory proposal = Proposal(
             _description,
             0,
@@ -50,10 +56,13 @@ contract VotingContract {
         emit CreateProposal(proposalCount);
     }
 
-    function castVote(uint256 proposalId, bool isApproved) public checkProposalEnded(proposalId){
+    function castVote(
+        uint256 proposalId,
+        bool isApproved
+    ) public checkProposalEnded(proposalId) {
         require(!hasVoted[msg.sender][proposalId], "Already voted");
         uint256 totalToken = votingToken.balanceOf(msg.sender);
-        if(isApproved){
+        if (isApproved) {
             proposals[proposalId].yesCount += totalToken;
         } else {
             proposals[proposalId].noCount += totalToken;
@@ -63,10 +72,13 @@ contract VotingContract {
     }
 
     function finalizeProposal(uint256 proposalId) public {
-        require(block.timestamp >= proposals[proposalId].timestamp, "Not ended");
+        require(
+            block.timestamp >= proposals[proposalId].timestamp,
+            "Not ended"
+        );
         uint256 yesCount = proposals[proposalId].yesCount;
         uint256 totalSupply = votingToken.totalSupply();
-        uint256 totalYesVote = yesCount/totalSupply * 100;
+        uint256 totalYesVote = (yesCount / totalSupply) * 100;
         if (totalYesVote > 50) {
             resultProposal[proposalId] = 1;
         } else {
@@ -74,5 +86,4 @@ contract VotingContract {
         }
         emit Finalize(proposalId, resultProposal[proposalId]);
     }
- 
 }
